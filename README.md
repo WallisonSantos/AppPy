6e7c3a71027bc54272e9ac08d9a03b0a16aae637
WallisonSantos <wallisontiagoassis206@outlook.com>
2022-10-12 23:53:56 -0300

    End job

--- README.md
+++ README.md
@@ -7,3 +7,6 @@
 ### digitar para ativar o ambiente virtual -  activate.bat
 
 ### digitar para desativar o ambiente virtual -  deactivate.bat
+
+### Executar arquivo -  C:/../Documentos/AppPy/env/Scripts/python.exe app.py
+
--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print('Hello world !')
+'''
+#  Aula   01
+#  Módulo Básico
+'''
+print('@wallison', 'santos', sep='-', end=' enviar para ')
+print('\n@kareen', 'souza', sep='-', end=' ')
+print('\ndados de cpf: ', sep='-', end=' ')
+print('824','176','070', sep='.', end='-18')
