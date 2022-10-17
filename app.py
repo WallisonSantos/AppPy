2e70bf12f024c933cf0564eff68300ee949ee955
WallisonSantos <wallisontiagoassis206@outlook.com>
2022-10-17 07:51:00 -0300

    Start
    Start Job !

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #  Aula   01
 #  Módulo Básico
+#  Formatação de strings
 #
 print('\n@wallison', 'santos', sep='-', end=' enviar para ')
 print('\n@kareen', 'assis', sep='.', end=' via outlook')
@@ -16,6 +17,10 @@ print( 'idade:{i} nascimeto:{n} peso:{m}'.format( i=idade, n=nasc, m=peso ))
 print(f'idade:{idade} nascimeto:{nasc} peso:{peso}')
 print(f'idade:{idade} nascimeto:{peso}   imc:{imc:.2f}')
 #
+#   Aula   02
+#   Módulo Básico
+#   Condicionais e Operadores relacionais
+#
 # - Menos de 18,5 - abaixo do peso
 # - 18,5 a 24,9 - peso saudável
 # - 25 a 29,9 - sobrepeso
@@ -24,12 +29,12 @@ print(f'idade:{idade} nascimeto:{peso}   imc:{imc:.2f}')
 #
 def warn(string):
     print(
-        "[\033[33m w \033[0m] {}".format(string)
+        "[\033[31m Caution \033[0m] {}".format(string)
     )
 #
 def error(string):
     print(
-        "[\033[31m ! \033[0m] {}".format(string)
+        "[\033[31m !!! \033[0m] {}".format(string)
     )
 #
 def error2(string):
@@ -37,7 +42,9 @@ def error2(string):
         "[\033[31m OK \033[0m] {}".format(string)
     )
 #
-print("")
+#
+print("# Condicionais e Operadores relacionais")
+#
 #
 if (imc == 18.5):
     print("medida está abaixo")
@@ -53,6 +60,8 @@ elif (imc >= 29.9 and imc < 39.9):
     error(imc)
 elif (imc >= 39.9):
     print("muito obeso (também conhecido como obeso mórbido)")
-    error(imc)
+    warn(imc)
 else:
     print("tente novamente")
+#
+#
