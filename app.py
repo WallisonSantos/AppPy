7d4609d21b4fd1c24baf191535938409868050ba
WallisonSantos <wallisontiagoassis206@outlook.com>
2022-10-17 07:56:27 -0300

    Cmt

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
 #  Aula   01
 #  Módulo Básico
-#  Formatação de strings
+#
+print("# Formatação de strings")
+#
 #
 print('\n@wallison', 'santos', sep='-', end=' enviar para ')
 print('\n@kareen', 'assis', sep='.', end=' via outlook')
 print('\n')
 #
 idade = int(input("Qual a sua idade ? "))
-peso  = int(input("Qual o seu peso ? "))
-altu  = float(input("Qual a sua altura ? "))
-nome  = input("Qual o seu nome ? ")
-imc   = peso / (altu * altu)
-nasc  = 2022-idade
+peso = int(input("Qual o seu peso ? "))
+altu = float(input("Qual a sua altura ? "))
+nome = input("Qual o seu nome ? ")
+imc = peso / (altu * altu)
+nasc = 2022-idade
 #
-print( 'idade:{i} nascimeto:{n} peso:{m}'.format( i=idade, n=nasc, m=peso ))
+print('idade:{i} nascimeto:{n} peso:{m}'.format(i=idade, n=nasc, m=peso))
 print(f'idade:{idade} nascimeto:{nasc} peso:{peso}')
 print(f'idade:{idade} nascimeto:{peso}   imc:{imc:.2f}')
 #
 #   Aula   02
 #   Módulo Básico
-#   Condicionais e Operadores relacionais
 #
 # - Menos de 18,5 - abaixo do peso
 # - 18,5 a 24,9 - peso saudável
@@ -28,19 +29,19 @@ print(f'idade:{idade} nascimeto:{peso}   imc:{imc:.2f}')
 # - + de 40 - muito obeso (também conhecido como obeso mórbido)
 #
 def warn(string):
-    print(
-        "[\033[31m Caution \033[0m] {}".format(string)
-    )
+   print(
+      "[\033[31m Caution \033[0m] {}".format(string)
+   )
 #
 def error(string):
-    print(
-        "[\033[31m !!! \033[0m] {}".format(string)
-    )
+   print(
+      "[\033[31m !!! \033[0m] {}".format(string)
+   )
 #
 def error2(string):
-    print(
-        "[\033[31m OK \033[0m] {}".format(string)
-    )
+   print(
+      "[\033[31m OK \033[0m] {}".format(string)
+   )
 #
 #
 print("# Condicionais e Operadores relacionais")
